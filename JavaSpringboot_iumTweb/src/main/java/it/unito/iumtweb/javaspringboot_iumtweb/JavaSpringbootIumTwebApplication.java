138dc737ac9c3c551b1ceef94892f66c6cfd5112
Riccardo Oro <riccardo.oro@edu.unito.it>
2024-04-14 16:52:54 +0200

    Init JavaSpringBoot server ,this server will run on port 8081

---
+++ JavaSpringboot_iumTweb/src/main/java/it/unito/iumtweb/javaspringboot_iumtweb/JavaSpringbootIumTwebApplication.java
@@ -0,0 +1,13 @@
+package it.unito.iumtweb.javaspringboot_iumtweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootIumTwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootIumTwebApplication.class, args);
+    }
+
+}
---
+++ JavaSpringboot_iumTweb/src/test/java/it/unito/iumtweb/javaspringboot_iumtweb/JavaSpringbootIumTwebApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unito.iumtweb.javaspringboot_iumtweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootIumTwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
