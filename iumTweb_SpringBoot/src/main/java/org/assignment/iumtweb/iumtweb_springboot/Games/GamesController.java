917f678adbb2cb63ecaa1144967d106ab9ca4834
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-05-28 18:24:17 +0200

    commented out a thing

--- iumTweb_SpringBoot/src/main/java/org/assignment/iumtweb/iumtweb_springboot/Games/GamesController.java
+++ iumTweb_SpringBoot/src/main/java/org/assignment/iumtweb/iumtweb_springboot/Games/GamesController.java
@@ -1,4 +1,23 @@
 package org.assignment.iumtweb.iumtweb_springboot.Games;
 
+import org.assignment.iumtweb.iumtweb_springboot.Games.GamesController;
+import org.assignment.iumtweb.iumtweb_springboot.Games.GamesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Games")
 public class GamesController {
+    private final GamesService gamesService;
+    /*
+    @Autowired
+    public GamesController(GamesService gamesService) {
+        this.gamesService = gamesService;
+    }
+
+     */
+
+
 }
